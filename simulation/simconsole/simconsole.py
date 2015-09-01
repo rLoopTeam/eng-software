300059e43ddb779d3b490d7440ef3376139bb304
Zane Harnish <public@zaneharnish.com>
2015-09-01 15:54:49 -0400

    Add simple menu for selecting tests to run

--- simulation/simconsole/simconsole.py
+++ simulation/simconsole/simconsole.py
@@ -1,1 +1,27 @@
 #Console app that lets users issue commands and request tests.
+
+#Main menu
+print "Choose a test to run, or enter 0 to run all:"
+print "0. Run All"
+print "1. Test A"
+print "2. Test B"
+print "3. Test C"
+
+while True:
+    selection = raw_input("Enter your selection: ")
+    if selection == "0":
+        print "Test A"
+        print "Test B"
+        print "Test C"
+        break
+    elif selection == "1":
+        print "Test A"
+        break
+    elif selection == "2":
+        print "Test B"
+        break
+    elif selection == "3":
+        print "Test C"
+        break
+    else:
+        print "Invalid selection."
