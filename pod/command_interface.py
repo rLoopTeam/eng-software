860c2f3dafc9392247cc24818ea2938507f9740b
Darren Midkiff <d.claymidkiff@gmail.com>
2015-09-11 17:49:30 -0500

    added boilerplate for command_interface

---
+++ pod/command_interface.py
@@ -0,0 +1,9 @@
+# Sub-Node Registration Interface
+# Author: Darren Midkiff <d.claymidkiff@gmail.com>
+# rLoop Software Engineering Team
+
+def attach_to_bus():
+  return
+
+def collect_logs():
+  return
