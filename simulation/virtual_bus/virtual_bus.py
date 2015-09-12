dde8a70d78d5f203e31dc16ab6f3c3a49b23a1ae
Michael Strein <mgstrein@gmail.com>
2015-09-11 20:03:24 -0700

    add doc comments to the bus methods

--- simulation/virtual_bus/virtual_bus.py
+++ simulation/virtual_bus/virtual_bus.py
@@ -6,13 +6,22 @@ class VirtualBus(object):
 		self.listeners = [];
 
 	def add_node(self, node):
+		"""Attach a node to the bus.
+
+		A Node is any object that has a method 'hear_message' available for the
+		bus to call when one of the attached nodes sends a message to the bus.
+		All nodes including the sender will hear the message.
+		"""
+
 		self.listeners.append(node);
 
 	def print_listeners(self):
+		"""Print the nodes attached to the bus"""
 		for listener in self.listeners:
 			print "Listener: %s" % listener
 
 	def send_message(self, msg):
+		"""Send a message to all of the nodes attached to the virtual bus"""
 		for listener in self.listeners:
 			listener.hear_message(msg)
 
