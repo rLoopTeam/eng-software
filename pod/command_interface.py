31caac1093349f8aa850f524a7c0bdf735d4732c
Darren Midkiff <d.claymidkiff@gmail.com>
2015-09-12 04:06:37 -0500

    added body to collect_logs function in command_interface

--- pod/command_interface.py
+++ pod/command_interface.py
@@ -2,8 +2,22 @@
 # Author: Darren Midkiff <d.claymidkiff@gmail.com>
 # rLoop Software Engineering Team
 
+#dummy value
+nodes = []
+
 def attach_to_bus():
   return
 
+
 def collect_logs():
-  return
+  myLog = open("commandInterfaceLog.txt", "r")
+  newLog = open("log.txt", "r+")
+  newLog.write("Combined Log" + "\n \n" + myLog.read())
+  for node in nodes:
+  	newLog.write(node.get_log() + "\n \n")
+  log = newLog.read()
+  myLog.close()
+  newLog.close()
+  return log
+
+collect_logs()
