d8ca6c93bc5df39bbce55df5ee1f7dde90770a0a
Paul <paullehenaff@msn.com>
2015-09-18 16:29:54 +0100

    Updated logging/test

--- systems/tests/test_sensor_node.py
+++ systems/tests/test_sensor_node.py
@@ -5,9 +5,13 @@ def test_sensor_register(virtualbus):
     node = TestSensorNode('external_temp_node')
     bus = VirtualBus()
 
+    """
+    Register node
+    """
+    print(' ')
+    print(' ')
+    print('TEST: Register node')
     node.setup(bus)
-    msg_reg = 'get_log %s'%(node.id)
-    bus.send_message(msg_reg)
 
     """
     Registered?
@@ -17,10 +21,30 @@ def test_sensor_register(virtualbus):
         if a_node.id == node.id:
             node_is_attached = True
     assert node_is_attached
+    print('SUCCESS: Registered')
+
+    """
+    Check state of log
+    """
+    print(' ')
+    print(node.get_log())
+    print(' ')
+    assert len(node.log) == 3 #length of log at node registration
+
+    """
+    Send the get_log message
+    """
+    msg_reg = 'get_log %s'%(node.id)
+    bus.send_message(msg_reg)
 
 
     """
     Did node receive message?
     """
-    print( ('Bus: Did %r hear message? %r')%(node.id, node.current_task))
+    print( 'TEST: Did %r hear message?'%node.id)
     assert node.current_task
+    print('SUCCESS: Received message')
+
+    print(' ')
+    print(node.get_log())
+
