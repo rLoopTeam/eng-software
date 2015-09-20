909ab9e21bba065c8a60255cdd060dc6e2d7e661
Darren Midkiff <d.claymidkiff@gmail.com>
2015-09-20 10:08:37 -0500

    wrote test for command_interface

---
+++ pod/command_interface/command_interface.py
@@ -0,0 +1,23 @@
+# Command Interface
+# Author: Darren Midkiff <d.claymidkiff@gmail.com>
+# rLoop Software Engineering Team
+
+#dummy value
+nodes = []
+
+def attach_to_bus():
+  return
+
+
+def collect_logs():
+  myLog = open("commandInterfaceLog.txt", "r")
+  newLog = open("log.txt", "r+")
+  newLog.write("Combined Log" + "\n \n" + myLog.read())
+  for node in nodes:
+  	newLog.write(node.get_log() + "\n \n")
+  log = newLog.read()
+  myLog.close()
+  newLog.close()
+  return log
+
+collect_logs()
