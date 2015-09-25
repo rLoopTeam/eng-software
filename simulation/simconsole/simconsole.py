a7e85d503b1db63ffe539b83663bbe1117937056
Michael Strein <mgstrein@gmail.com>
2015-09-24 20:53:57 -0700

    modified the simconsole to be able to construct a simulation of the hyperloop software

--- simulation/simconsole/simconsole.py
+++ simulation/simconsole/simconsole.py
@@ -1,27 +1,36 @@
 #Console app that lets users issue commands and request tests.
 
+from pod.pod_control import PodControl
+from simulation.virtual_bus.virtual_bus import VirtualBus
+from systems.nodes.basic_node import BasicNode
+
+# default objects
+virtual_bus = VirtualBus()
+pod_control = PodControl()
+
+basic_nodes_list = []
+
+#Initialize virtual bus and pod control
+pod_control.set_bus(virtual_bus)
+
 #Main menu
 print "Choose a test to run, or enter 0 to run all:"
-print "0. Run All"
-print "1. Test A"
-print "2. Test B"
-print "3. Test C"
+print "0. Add basic node"
+print "1. Request State Report"
+print "2. Collect Logs"
 
 while True:
     selection = raw_input("Enter your selection: ")
     if selection == "0":
-        print "Test A"
-        print "Test B"
-        print "Test C"
-        break
+        basic_node = BasicNode()
+
+        basic_nodes_list.append(basic_node)
+
+        basic_node.set_bus(virtual_bus)
+        basic_node.register_with_pod_controller()
     elif selection == "1":
-        print "Test A"
-        break
+        pod_control.report_state()
     elif selection == "2":
-        print "Test B"
-        break
-    elif selection == "3":
-        print "Test C"
-        break
+        pod_control.collect_logs()
     else:
         print "Invalid selection."
